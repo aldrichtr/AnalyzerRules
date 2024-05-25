7eae31b12136cc717ad4425442a1c8aee5810a60
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2024-05-24 23:08:14 -0400

    feat: new base function for case tests

--- source/AnalyzerHelpers/public/StringCase/Test-Case.ps1
+++ source/AnalyzerHelpers/public/StringCase/Test-Case.ps1
@@ -1,3 +1,5 @@
+using namespace System.Text
+
 
 function Test-Case {
     <#
@@ -51,15 +53,56 @@ function Test-Case {
     )
     begin {
         Write-Debug "`n$('-' * 80)`n-- Begin $($MyInvocation.MyCommand.Name)`n$('-' * 80)"
-        $lowerWithDigit = '[a-z0-9]'
-        $lowerNoDigit   = '[a-z]'
-        $upperWithDigit = '[A-Z0-9]'
-        $upperNoDigit   = '[A-Z]'
         $up             = 1
         $cancelChars    = @('.', ' ', '-', '_')
+
+        $pattern = [StringBuilder]::new()
+
+        function Set-Pattern {
+            param(
+                [string]$case,
+                [switch]$DontAllowDigits
+            )
+            if ($DontAllowDigits) {
+                $lower     = '[a-z]'
+                $upper     = '[A-Z]'
+                $any       = '[a-zA-Z]'
+            } else {
+                $lower   = '[a-z0-9]'
+                $upper   = '[A-Z0-9]'
+                $any       = '[a-zA-Z0-9]'
+            }
+            switch ($case) {
+                'upper' { $upper }
+                'lower' { $lower }
+                'capital' { "$upper$lower+" }
+                'startLower' { "$lower$any+"}
+                'startUpper' { "$upper$any+"}
+            }
+        }
     }
     process {
         #TODO: Refactor all Test-*Case functions to use this based on parameters
+        [void]$pattern.Append('^')
+        $wordCasePattern = (Set-Pattern $WordCase $DontAllowDigits)
+        if ($PSBoundParameters.ContainsKey('FirstWordCase')) {
+            [void]$pattern.Append( (Set-Pattern $FirstWordCase $DontAllowDigits) )
+        } else {
+            [void]$pattern.Append($wordCasePattern)
+        }
+
+        [void]$pattern.Append($Separator)
+        [void]$pattern.Append($wordCasePattern)
+        [void]$pattern.AppendJoin('', @(
+            '(',
+            $Separator,
+            $wordCasePattern,
+            ')'
+        ))
+        [void]$pattern.Append('$')
+
+        Write-Debug "Using Pattern: '$($pattern.ToString())"
+        $InputObject -cmatch $pattern.ToString()
     }
     end {
         Write-Debug "`n$('-' * 80)`n-- End $($MyInvocation.MyCommand.Name)`n$('-' * 80)"
