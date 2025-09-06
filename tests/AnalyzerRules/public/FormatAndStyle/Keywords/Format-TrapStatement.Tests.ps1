71da9fcb6afcf45c1a905f4eea6bc33c104ca6f3
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2025-09-05 20:54:18 -0400

    task(saveWIP): Creating a restore point before continuing

--- tests/AnalyzerRules/public/FormatAndStyle/Keywords/Format-TrapStatement.Tests.ps1
+++ tests/AnalyzerRules/public/FormatAndStyle/Keywords/Format-TrapStatement.Tests.ps1
@@ -18,7 +18,6 @@ BeforeDiscovery {
 
 }
 
-
 $options = @{
   Name    = 'GIVEN the public function Format-TrapStatement'
   Tag     = @(
@@ -29,65 +28,66 @@ $options = @{
   Foreach = $sourceFile
 }
 Describe @options {
-  BeforeAll {
-    $sourceFile = $_
-  }
-  Context 'WHEN The function is sourced in the current environment' -Tag @('fileParse') {
-    BeforeAll {
-      $tokens      = $null
-      $parseErrors = $null
-      $result = [Parser]::ParseFile($sourceFile, [ref ]$tokens, [ref ]$parseErrors)
+  BeforeAll { $sourceFile = $_ }
+  Context 'SCENARIO: The function can be parsed and loaded without error' {
+    Context 'WHEN The function is sourced in the current environment' -Tag @('fileParse') {
+      BeforeAll {
+        $tokens      = $null
+        $parseErrors = $null
+        $result = [Parser]::ParseFile($sourceFile, [ref ]$tokens, [ref ]$parseErrors)
+      }
+
+      It 'THEN it should parse without error' {
+        $parseErrors | Should -BeNullOrEmpty
+      }
+      It 'THEN it should load without error' {
+        (Get-Command 'Format-TrapStatement') | Should -Not -BeNullOrEmpty
+      }
     }
 
-    It 'THEN it should parse without error' {
-      $parseErrors | Should -BeNullOrEmpty
-    }
-    It 'THEN it should load without error' {
-      (Get-Command 'Format-TrapStatement') | Should -Not -BeNullOrEmpty
-    }
-  }
+    Context 'WHEN the <rule.RuleName> rule is tested' -Tag @('fileAnalysis') -ForEach $analyzerRules {
+      BeforeAll {
+        # Rename automatic variable to rule to make it easier to work with
+        $rule = $_
+      }
 
-  Context 'WHEN the <rule.RuleName> rule is tested' -Tag @('fileAnalysis') -ForEach $analyzerRules {
-    BeforeAll {
-      # Rename automatic variable to rule to make it easier to work with
-      $rule = $_
-    }
-
-    It 'THEN it should pass' {
-      $analysis | Should -Pass $rule
+      It 'THEN it should pass' {
+        $analysis | Should -Pass $rule
+      }
     }
   }
 
   <# --=-- #>
-  Context 'GIVEN that we pass a scriptblock to Format-TrapStatement outside ScriptAnalyzer' -Tag @("Raw") -ForEach @(
-    @{
-      ScriptBlock = @'
+  Context 'SCENARIO: Use the function to analyze scriptblocks' {
+    Context 'GIVEN that we pass a scriptblock to Format-TrapStatement outside ScriptAnalyzer' -Tag @('Raw') -ForEach @(
+      @{
+        ScriptBlock = @'
 { TRAP { Write-Warning "Yikes, a trap!"}}
 '@
-      ResultCount = 1
-    }
-  ) {
-    BeforeAll {
-      Mock Get-RuleSetting -ModuleName AnalyzerRules -MockWith {
-        return @{
-          Enabled = $true
-          Case    = 'lower'
-        }
+        ResultCount = 1
       }
+    ) {
+      BeforeAll {
+        Mock Get-RuleSetting -ModuleName AnalyzerRules -MockWith {
+          return @{
+            Enabled = $true
+            Case    = 'lower'
+          }
+        }
 
-      $scriptAst = ConvertTo-Ast $ScriptBlock
-      $trapAst = $scriptAst.Find({ param($ast) $ast -is [TrapStatementAst] }, $true)
-      $result = Format-TrapStatement -TrapStatementAst $trapAst
+        $scriptAst = ConvertTo-Ast $ScriptBlock
+        $trapAst = $scriptAst.Find({ param($ast) $ast -is [TrapStatementAst] }, $true)
+        $result = Format-TrapStatement -InputAst $trapAst
+      }
+      It 'It should have <ResultCount> Results' {
+        $result.Count | Should -Be $ResultCount
+      }
     }
-    It 'It should have <ResultCount> Results' {
-      $result.Count | Should -Be $ResultCount
-    }
-  }
 
-  Context 'When ScriptAnalyzer is called with <AnalyzerOptions.IncludeRule>' -ForEach @(
-    @{
-      AnalyzerOptions = @{
-        ScriptDefinition      = @'
+    Context 'When ScriptAnalyzer is called with <AnalyzerOptions.IncludeRule>' -ForEach @(
+      @{
+        AnalyzerOptions = @{
+          ScriptDefinition      = @'
 function Test-CaseOfTrap {
 [CmdletBinding()]
 param()
@@ -95,24 +95,24 @@ begin {}
 process { Trap { Write-Warning "Yikes, a trap!"}}
 }
 '@
-        CustomRulePath        = 'stage'
-        RecurseCustomRulePath = $true
-        IncludeDefaultRules   = $false
-        IncludeRule           = 'Format-TrapStatement'
-        Settings              = @{
-          Rules = @{
-            FormatTrapStatement = @{
-              Enabled = $true
-              Case    = 'lower'
+          CustomRulePath        = 'stage'
+          RecurseCustomRulePath = $true
+          IncludeDefaultRules   = $false
+          IncludeRule           = 'Format-TrapStatement'
+          Settings              = @{
+            Rules = @{
+              FormatTrapStatement = @{
+                Enabled = $true
+                Case    = 'lower'
+              }
             }
           }
         }
-      }
-      ResultCount     = 1
-    },
-    @{
-      AnalyzerOptions = @{
-        ScriptDefinition      = @'
+        ResultCount     = 1
+      },
+      @{
+        AnalyzerOptions = @{
+          ScriptDefinition      = @'
 function Test-CaseOfTrap1 {
 [CmdletBinding()]
 param()
@@ -120,30 +120,31 @@ begin {}
 process { TRAP { Write-Warning "Yikes, a trap!"}}
 }
 '@
-        CustomRulePath        = 'stage'
-        RecurseCustomRulePath = $true
-        IncludeDefaultRules   = $false
-        IncludeRule           = 'Format-TrapStatement'
-        Settings              = @{
-          Rules = @{
-            FormatTrapStatement = @{
-              Enabled = $true
-              Case    = 'lower'
+          CustomRulePath        = 'stage'
+          RecurseCustomRulePath = $true
+          IncludeDefaultRules   = $false
+          IncludeRule           = 'Format-TrapStatement'
+          Settings              = @{
+            Rules = @{
+              FormatTrapStatement = @{
+                Enabled = $true
+                Case    = 'lower'
+              }
             }
           }
         }
+        ResultCount     = 1
       }
-      ResultCount     = 1
-    }
-  ) {
-    BeforeEach {
-      $result = Invoke-ScriptAnalyzer @AnalyzerOptions
-    }
+    ) {
+      BeforeEach {
+        $result = Invoke-ScriptAnalyzer @AnalyzerOptions
+      }
 
-    It 'It should have a result count of <ResultCount>' {
-      $result.Count | Should -Be $ResultCount
-    }
+      It 'It should have a result count of <ResultCount>' {
+        $result.Count | Should -Be $ResultCount
+      }
 
+    }
   }
 
   <# --=-- #>
